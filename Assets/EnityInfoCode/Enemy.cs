a4fa8c9ca00a2dd9d51ab8b0c1d0ea9872e5bde8
Jake Milam <jmilam@chapman.edu>
2025-12-10 14:04:14 -0800

    waterball now bouncy, empty enemy object to fix errors,
    also new bools for projectiles for destroy on wall or enemy.

--- Assets/EnityInfoCode/Enemy.cs
+++ Assets/EnityInfoCode/Enemy.cs
@@ -24,7 +24,7 @@ public class Enemy : MonoBehaviour
     
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Projectile") )
+        if (collision.gameObject.CompareTag("Projectile"))
         {
             LoseHealth(10);
         }
